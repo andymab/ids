2c65b3ec88c27360b9377f5b69db40e634922cbb
_Andy_ <650517@355042.ru>
2023-11-09 16:55:52 +0300

    w

---
+++ src/components/Photo/FilePreviewDialog.vue
@@ -0,0 +1,139 @@
+<template>
+    <v-dialog v-model="showPreview" width="auto">
+        <v-card>
+            <v-card-title>File Upload Preview.</v-card-title>
+            <v-card-text>
+                <div class="d-flex flex-column justify-center align-center fill-height">
+
+                    <input ref="filePickerField" type="file" accept="image/*" @change="launchCropper" hidden />
+
+                    <div>
+                        <v-avatar size="350px" class="mt-5" style="border: 2px solid black;">
+                            <v-img :src="avatarImage" @click="uploadBlob(avatarImage)" class="avatar-image"></v-img>
+                        </v-avatar>
+                    </div>
+
+                    <v-btn class="mt-5" @click="$refs.filePickerField.click()"> Upload </v-btn>
+
+                    <image-cropper-dialog ref="cropperDialog" :chosenImage="chosenImage"
+                        @onReset="$refs.filePickerField.value = null" @onCrop="(croppedImage) => {
+                            avatarImage = croppedImage;
+                        }" />
+                </div>
+                <!-- <img v-bind:src="imagePreview" v-show="showPreview" /> -->
+                <!-- <input type="file" id="file" ref="file" accept="image/*" v-on:change="handleFileUpload()"/> -->
+                <!-- <button v-on:click="submitFile()">Submit</button> -->
+            </v-card-text>
+            <!-- <v-card-actions>
+                <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+            </v-card-actions> -->
+        </v-card>
+    </v-dialog>
+</template>
+
+
+<script>
+import axios from 'axios'
+import ImageCropperDialog from './ImageCropperDialog.vue';
+import defImage from "@/assets/default.jpg"
+
+
+export default {
+    name: 'FilePreviewDialog',
+    components: {
+        ImageCropperDialog
+    },
+    props: {
+        dialog: {
+            type: Boolean,
+            default: () => false
+        },
+    },
+    data() {
+        return {
+            file: '',
+            showPreview: false,
+            imagePreview: '',
+            avatarImage: defImage,
+            chosenImage: null,
+        }
+    },
+    created() {
+        this.showPreview = this.dialog;
+    },
+    watch: {
+        dialog: function () {
+            console.log('---' + this.dialog);
+            this.showPreview = this.dialog;
+        },
+
+    },
+    methods: {
+        async uploadBlob(base64) {
+
+
+            var link = document.createElement("a");
+            document.body.appendChild(link); // for Firefox
+            link.setAttribute("href", base64);
+            link.setAttribute("download", 'download.jpg');
+            link.click();
+
+
+
+        },
+        async launchCropper(event) {
+            if (!event) return;
+            var file = event.target.files[0];
+            this.chosenImage = await this.toBase64(file);
+            this.$refs.cropperDialog.initCropper(file.type);
+        },
+
+        async toBase64(file) {
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => resolve(reader.result);
+                reader.onerror = error => reject(error);
+            });
+        },
+
+        handleFileUpload() {
+            this.file = this.$refs.file.files[0];
+            let reader = new FileReader();
+            reader.addEventListener("load", function () {
+                this.showPreview = true;
+                this.imagePreview = reader.result;
+            }.bind(this), false);
+            if (this.file) {
+                if (/\.(jpe?g|png|gif)$/i.test(this.file.name)) {
+                    reader.readAsDataURL(this.file);
+                }
+            }
+        },
+        submitFile() {
+            let formData = new FormData();
+            formData.append('file', this.file);
+            axios.post('/file-preview',
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function () {
+                console.log('SUCCESS!!');
+            })
+                .catch(function () {
+                    console.log('FAILURE!!');
+                });
+        },
+    }
+}
+</script>
+
+<style scope>
+.container img {
+    max-width: 200px;
+    max-height: 200px;
+}
+</style>
--- src/views/ImagesView.vue
+++ src/views/ImagesView.vue
@@ -8,27 +8,24 @@
 
         <v-tooltip location="top">
           <template v-slot:activator="{ props: tooltip }">
-            <v-btn icon="mdi-newspaper-plus" v-bind="mergeProps(tooltip)" @click="showFilePreview = !showFilePreview"></v-btn>
+            <v-btn icon="mdi-newspaper-plus" v-bind="mergeProps(tooltip)"
+              @click="showFilePreview = !showFilePreview"></v-btn>
           </template>
           <span>Новый альбом</span>
         </v-tooltip>
 
-    
-          <v-switch v-model="showtooltype" hide-details inset compact label="Показать описания"></v-switch>
+
+        <v-switch v-model="showtooltype" hide-details inset compact label="Показать описания"></v-switch>
       </v-toolbar>
       <div>
-      <file-preview :dialog="showFilePreview"/>
-    </div>
+        <file-preview-dialog :dialog="showFilePreview" @onReset="showFilePreview = false" />
+      </div>
       <v-virtual-scroll :items="items" height="dynamic">
         <template v-slot:default="{ item }">
           <div class="row">
             <div v-for="n in item" class="image-block">
               <div class="image-content">
-                <div class="image-text-block" v-show="showtooltype">
-                  <h6>{{ n.title }}</h6>
-                </div>
                 <div class="image-item">
-
                   <RouterLink to="/images/1">
                     <v-img :src="n.src" :lazy-src="n.src" contain class="bg-grey-lighten-2">
                       <template v-slot:placeholder>
@@ -36,6 +33,11 @@
                           <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
                         </v-row>
                       </template>
+                      <transition name="fade">
+                        <div class="image-text-block" v-if="showtooltype">
+                          <h6>{{ n.title }}</h6>
+                        </div>
+                      </transition>
                     </v-img>
                   </RouterLink>
                 </div>
@@ -52,19 +54,19 @@
 
 import { mergeProps } from 'vue'
 
-import FilePreview from '../components/Photo/FilePreview.vue' 
+import FilePreviewDialog from '../components/Photo/FilePreviewDialog.vue'
 
 
 import json from '/src/assets/photo/albom.json'
 
 export default {
-  components:{
-    FilePreview
+  components: {
+    FilePreviewDialog
   },
   data: () => ({
     items: json,
-    showFilePreview:false,
-    showtooltype:false,
+    showFilePreview: false,
+    showtooltype: false,
     breadcrumbs: [
       {
         icon: 'mdi-view-dashboard',
@@ -102,7 +104,7 @@ export default {
   flex: 25%;
   max-width: 25%;
   padding: 0 4px;
-  background-color: black;
+  /* background-color: black; */
   text-align: center;
 }
 
@@ -134,7 +136,7 @@ export default {
   opacity: 0.7;
   background-color: rgb(19, 18, 18);
   position: absolute;
-  bottom: 4px;
+  bottom: 0;
   z-index: 2;
   font-size: 24px;
 }
